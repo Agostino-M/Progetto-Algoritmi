7cb851fd11a879059317f1dbe0abbc012610bd57
Agostino Messina <agostino.messina@edu.unito.it>
2021-05-22 19:27:20 +0200

    Upload UnionFindSetException

---
+++ Esercizio 3/src/UnionFindSetException.java
@@ -0,0 +1,12 @@
+/**
+ * Exception throwable by the UnionFindSet library
+ * 
+ * @author Agostino
+ */
+public class UnionFindSetException extends Exception {
+
+    public UnionFindSetException(String message) {
+        super(message);
+    }
+
+}
