e4cece63f872033e161c7f248d071cf8cba9c0be
Agostino Messina <agostino.messina@edu.unito.it>
2021-05-30 18:31:12 +0200

    Add more tests

--- Esercizio 4/src/graph/GraphTests.java
+++ Esercizio 4/src/graph/GraphTests.java
@@ -16,13 +16,15 @@ import org.junit.Before;
  */
 public class GraphTests {
 
-    private Graph<Integer> graphInt;
-    private Graph<String> graphString;
+    private Graph<Integer, Integer> graphInt;
+    private Graph<String, String> graphString;
+    private Graph<String, Integer> graphIntString;
 
     @Before
     public void createUnionFindSets() {
         graphInt = new Graph<>(true);
         graphString = new Graph<>(true);
+        graphIntString = new Graph<>(true);
 
         graphInt.addEdge(0, 1, 5);
         graphInt.addEdge(0, 4, 4);
@@ -39,12 +41,20 @@ public class GraphTests {
         graphString.addEdge("Sperando", "Che", "il");
         graphString.addEdge("mio", "Prof", "preferito");
         graphString.addEdge("non", "mi", "BOCCI");
+
+        graphIntString.addEdge("Ciao", "Amici", 10);
+        graphIntString.addEdge("fate", "io", 2);
+        graphIntString.addEdge("studio", "algoritmi", 5);
+        graphIntString.addEdge("mi", "piace", 4);
+        graphIntString.addEdge("Sperando", "Che", 4);
+        graphIntString.addEdge("mio", "Prof", 8);
+        graphIntString.addEdge("non", "mi", 1);
     }
 
     @Test
     public void testZeroNodesInt() {
         graphInt = new Graph<>(false);
-        
+
         assertEquals(graphInt.getNodeCount(), 0);
     }
 
@@ -53,6 +63,13 @@ public class GraphTests {
         graphInt = new Graph<>(false);
 
         assertEquals(graphInt.getEdgesCount(), 0);
+    }
+
+    @Test
+    public void testZeroNodesIntString() {
+        graphIntString = new Graph<>(false);
+
+        assertEquals(graphIntString.getNodeCount(), 0);
     }
 
     @Test
@@ -78,6 +95,17 @@ public class GraphTests {
     }
 
     @Test
+    public void testAddNodeIntString() {
+        graphIntString = new Graph<>(false);
+        graphIntString.addNode("Albero");
+
+        List<String> arrayExpected = new ArrayList<>();
+        arrayExpected.add("Albero");
+
+        assertEquals(graphIntString.getNodes(), arrayExpected);
+    }
+
+    @Test
     public void testAddEdgeInt() {
         assertEquals(graphInt.getEdgesCount(), 7);
     }
@@ -98,6 +126,11 @@ public class GraphTests {
     }
 
     @Test
+    public void testHasNodeIntString() {
+        assertTrue(graphIntString.hasNode("Ciao"));
+    }
+
+    @Test
     public void testDeleteNodeInt() {
         graphInt.deleteNode(2);
         assertFalse(graphInt.hasNode(2));
@@ -107,6 +140,12 @@ public class GraphTests {
     public void testDeleteNodeString() {
         graphString.deleteNode("Ciao");
         assertFalse(graphString.hasNode("Ciao"));
+    }
+
+    @Test
+    public void testDeleteNodeIntString() {
+        graphIntString.deleteNode("Ciao");
+        assertFalse(graphIntString.hasNode("Ciao"));
     }
 
     @Test
@@ -122,8 +161,14 @@ public class GraphTests {
     }
 
     @Test
+    public void testDeleteEdgeIntString() {
+        graphIntString.deleteEdge("Ciao", "Amici");
+        assertFalse(graphIntString.hasEdge("Ciao", "Amici"));
+    }
+
+    @Test
     public void testAdjacentNodesInt() {
-        List<Edge<Integer>> expectedList = new ArrayList<>();
+        List<Edge<Integer, Integer>> expectedList = new ArrayList<>();
 
         expectedList.add(new Edge<>(0));
         expectedList.add(new Edge<>(2));
@@ -135,11 +180,21 @@ public class GraphTests {
 
     @Test
     public void testAdjacentNodesString() {
-        List<Edge<String>> expectedList = new ArrayList<>();
+        List<Edge<String, String>> expectedList = new ArrayList<>();
 
         expectedList.add(new Edge<>("piace"));
         expectedList.add(new Edge<>("non"));
 
         assertEquals(graphString.getAdjacentNode("mi"), expectedList);
+    }
+
+    @Test
+    public void testAdjacentNodesIntString() {
+        List<Edge<String, Integer>> expectedList = new ArrayList<>();
+
+        expectedList.add(new Edge<>("piace"));
+        expectedList.add(new Edge<>("non"));
+
+        assertEquals(graphIntString.getAdjacentNode("mi"), expectedList);
     }
 }
